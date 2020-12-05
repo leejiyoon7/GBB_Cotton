cb172774891e173de83bc93bd779dd41807327c2
MoNireu <monireu.dev@gmail.com>
2020-12-05 23:04:33 +0900

    feat: 도서거래 페이지 검색과 칩 추가 구현.

--- app/src/main/java/com/example/cotton/ui/Exchange/ExchangeFragment.java
+++ app/src/main/java/com/example/cotton/ui/Exchange/ExchangeFragment.java
@@ -1,25 +1,64 @@
 package com.example.cotton.ui.Exchange;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.cotton.R;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipDrawable;
+import com.google.android.material.chip.ChipGroup;
 
 public class ExchangeFragment extends Fragment {
 
 
+    ChipGroup chipGroup;
+    SearchView searchView;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_trading, container, false);
+        View root = inflater.inflate(R.layout.fragment_trading, container, false);
 
+        chipGroup = root.findViewById(R.id.trading_header_chip_group);
+        searchView = root.findViewById(R.id.trading_header_search_view);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
 
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d("검색창 = ", "OK");
+                Chip chip = new Chip(getContext(), null, R.style.Widget_MaterialComponents_Chip_Entry);
+                chip.setText(searchView.getQuery());
+                chip.setCheckable(false);
+                ChipDrawable drawable = ChipDrawable.createFromAttributes(getContext(), null, 0, R.style.Widget_MaterialComponents_Chip_Entry);
+                chip.setChipDrawable(drawable);
+                chip.setOnCloseIconClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        chipGroup.removeView(chip);
+                    }
+                });
+                chipGroup.addView(chip);
 
-        return view;
+                searchView.setQuery("", false);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return root;
     }
+
 }

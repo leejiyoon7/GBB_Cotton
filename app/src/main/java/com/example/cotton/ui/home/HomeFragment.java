32190e2536331640550ad1ef8834815ea31921cb
wjdddlf97 <wjdddlf97@naver.com>
2020-12-06 23:24:14 +0900

    feat : 홈화면 리스트 구현

--- app/src/main/java/com/example/cotton/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/cotton/ui/home/HomeFragment.java
@@ -259,36 +259,56 @@ public class HomeFragment extends Fragment {
     //대여 도서 목록 RecyclerView 설정
     public void showMyRentedBookListFunc(){
         //adapter 생성
-        myRentedBookListAdapter = new MyRentedBookListAdapter() ;
-        
-        home_my_rented_book_recycler_view.setLayoutManager(new LinearLayoutManager(getActivity()));
-        //adapter 달기
-        home_my_rented_book_recycler_view.setAdapter(myRentedBookListAdapter);
 
-        // 리사이클러뷰에 표시할 데이터 리스트 생성(예시로 3개), 추후 firebase에서 data 받아와 동적으로 생성되게 짤 예정
-        myRentedBookListAdapter.addItem("C언어 콘서트","김동주","도서대여");
-        myRentedBookListAdapter.addItem("Java 콘서트","김민석","연체");
-        myRentedBookListAdapter.addItem("C++ 콘서트","이정일","반납대기");
-        myRentedBookListAdapter.addItem("C# 콘서트","심민수","반납대기");
-        myRentedBookListAdapter.notifyDataSetChanged();//adapter의 변경을 알림
+
+        firebaseFunction firebaseTest = new firebaseFunction();
+
+        firebaseTest.myRentedBookListGet((resultList) -> {
+            myRentedBookListAdapter = new MyRentedBookListAdapter() ;
+            home_my_rented_book_recycler_view.setLayoutManager(new LinearLayoutManager(getActivity()));
+            //adapter 달기
+            home_my_rented_book_recycler_view.setAdapter(myRentedBookListAdapter);
+            for(int i=0;i<resultList.size();i++){
+                myRentedBookListAdapter.addItem(resultList.get(i).getBookName(),resultList.get(i).getBookWriter(),resultList.get(i).getStatus());
+            }
+            // 리사이클러뷰에 표시할 데이터 리스트 생성(예시로 3개), 추후 firebase에서 data 받아와 동적으로 생성되게 짤 예정
+            myRentedBookListAdapter.addItem("C언어 콘서트","김동주","도서대여");
+            myRentedBookListAdapter.addItem("Java 콘서트","김민석","연체");
+            myRentedBookListAdapter.addItem("C++ 콘서트","이정일","반납대기");
+            myRentedBookListAdapter.addItem("C# 콘서트","심민수","반납대기");
+            myRentedBookListAdapter.notifyDataSetChanged();//adapter의 변경을 알림
+            return null;
+        });
+
     }
 
     //나의 도서 목록 RecyclerView 설정
     public void showMyRegisteredBookFunc(){
         //adapter 생성
-        myRegisteredBookListAdapter = new MyRegisteredBookListAdapter() ;
 
-        home_my_registered_book_recycler_view.setLayoutManager(new LinearLayoutManager(getActivity()));
-        //adapter 달기
-        home_my_registered_book_recycler_view.setAdapter(myRegisteredBookListAdapter);
+
+        firebaseFunction firebaseTest = new firebaseFunction();
+        firebaseTest.myRegisteredBookListGet((resultList) -> { //resultList안에 너가 원하는 모든게 있단다.
+            myRegisteredBookListAdapter = new MyRegisteredBookListAdapter() ;
+
+            home_my_registered_book_recycler_view.setLayoutManager(new LinearLayoutManager(getActivity()));
+            //adapter 달기
+            home_my_registered_book_recycler_view.setAdapter(myRegisteredBookListAdapter);
+
+            for(int i=0;i<resultList.size();i++) {
+                myRegisteredBookListAdapter.addItem(resultList.get(i).getBookName(), resultList.get(i).getBookWriter());
+            }
+            myRegisteredBookListAdapter.addItem("알기 쉽게 해설한 데이터구조","김동주");
+            myRegisteredBookListAdapter.addItem("케라스 창시자에게 배우는 딥러닝","김민석");
+            myRegisteredBookListAdapter.addItem("JAVA Programming","이정일");
+
+            myRegisteredBookListAdapter.notifyDataSetChanged();//adapter의 변경을 알림
+            return null;
+        });
+
 
         // 리사이클러뷰에 표시할 데이터 리스트 생성(예시로 3개), 추후 firebase에서 data 받아와 동적으로 생성되게 짤 예정
-        myRegisteredBookListAdapter.addItem("알기 쉽게 해설한 데이터구조","김동주");
-        myRegisteredBookListAdapter.addItem("케라스 창시자에게 배우는 딥러닝","김민석");
-        myRegisteredBookListAdapter.addItem("JAVA Programming","이정일");
-        myRegisteredBookListAdapter.addItem("컴퓨터 아키텍쳐","심민수");
 
-        myRegisteredBookListAdapter.notifyDataSetChanged();//adapter의 변경을 알림
     }
 
     public void searchMoney(String wallet){

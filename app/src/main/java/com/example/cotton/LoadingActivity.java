104b8791325cd1388433c6d6f4e635c16fd28468
MoNireu <monireu.dev@gmail.com>
2020-12-07 23:58:32 +0900

    fix: Splash화면 동영상으로 교체.

--- app/src/main/java/com/example/cotton/LoadingActivity.java
+++ app/src/main/java/com/example/cotton/LoadingActivity.java
@@ -1,38 +1,57 @@
 package com.example.cotton;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
-import android.widget.ImageView;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 
 public class LoadingActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        try {
+            VideoView videoHolder = (VideoView)findViewById(R.id.loading_view);
+            Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.splash_video);
+            videoHolder.setVideoURI(video);
 
-        ImageView load = (ImageView)findViewById(R.id.loading_view);
-        Glide.with(this).load(R.raw.splash).into(load);
+            videoHolder.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    float videoRatio = mp.getVideoWidth() / (float) mp.getVideoHeight();
+                    float screenRatio = videoHolder.getWidth() / (float)
+                            videoHolder.getHeight();
+                    float scaleX = videoRatio / screenRatio;
+                    if (scaleX >= 1f) {
+                        videoHolder.setScaleX(scaleX);
+                    } else {
+                        videoHolder.setScaleY(1f / scaleX);
+                    }
+                }
+            });
 
-        startLoading();
+            videoHolder.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                public void onCompletion(MediaPlayer mp) {
+                    jump();
+                }
+            });
+            videoHolder.start();
+        } catch (Exception ex) {
+            jump();
+        }
     }
 
-    private void startLoading() {
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run(){
-                Intent intent = new Intent(getBaseContext(), MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        }, 4500);
-
+    private void jump() {
+        if (isFinishing())
+            return;
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }

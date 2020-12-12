b6a47090f5bc4d4f3e7002a98a53fe6550eb490a
DESKTOP-9DHRNC9\leeji <leejiyoon7@naver.com>
2020-12-12 22:46:30 +0900

    feat: floating button func add

--- app/src/main/java/com/example/cotton/ui/history/HistoryFragment.java
+++ app/src/main/java/com/example/cotton/ui/history/HistoryFragment.java
@@ -28,6 +28,7 @@ import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
@@ -60,6 +61,7 @@ public class HistoryFragment extends Fragment {
     List<MemberInfo> memberInfos = new ArrayList<>();
     List<LogForm> logFormList;
     DateCompare dateCompare;
+    FloatingActionButton floatingActionButton;
 
     FirebaseFunction firebaseFunction;//firebase log output
 
@@ -72,6 +74,17 @@ public class HistoryFragment extends Fragment {
         segmentedButtonGroup = (SegmentedButtonGroup)view.findViewById(R.id.segmentedButtonGroup);
 
         transactional_information_list=view.findViewById(R.id.transactional_information_listView);//listview 참조
+
+        floatingActionButton = view.findViewById(R.id.floatingActionButton);
+
+
+        //플로팅 버튼 온클릭 함수
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("플로팅 버튼","클릭");
+            }
+        });
 
         //segmentButtonGroup 버튼 클릭 이벤트(position)별, 추후 구현 예정
         segmentButtonClickEvent();
@@ -228,75 +241,4 @@ public class HistoryFragment extends Fragment {
             }
         });
     }
-
-
-
-
-    public String getPath(Uri uri){     //사진 경로받기
-        String[]proj = {MediaStore.Images.Media.DATA};
-        CursorLoader cursorLoader = new CursorLoader(getActivity(),uri,proj,null,null,null);
-
-        Cursor cursor = cursorLoader.loadInBackground();
-        int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-
-        cursor.moveToFirst();
-
-        return cursor.getString(index);
-    }
-
-    private void showGallery() {        //갤러리 띄우기
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent. setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
-        startActivityForResult(intent, 200);
-
-
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {        //사진 클릭시 화면에 보이기
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 200 && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            selectedImageUri = data.getData();
-            bookImg.setImageURI(selectedImageUri);
-        }
-    }
-
-    private void localUpload(String bookName, String major, String bookWriter) {         //업로드
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-
-        // Create a storage reference from our app
-        StorageReference storageRef = storage.getReference();
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        FirebaseFunction firebaseTest = new FirebaseFunction();
-
-        firebaseTest.profileGet(memberInfos, (resultList) -> {
-            Log.d("home에서 확인",resultList.get(0).getName());
-            Uri file = Uri.fromFile(new File(getPath(selectedImageUri)));
-            final StorageReference riversRef = storageRef.child("bookSave/" + bookName + "_" + resultList.get(0).getName());
-            UploadTask uploadTask = riversRef.putFile(file);
-
-            Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
-                @Override
-                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
-                    if (!task.isSuccessful()) {
-                        throw task.getException();
-                    }
-                    // Continue with the task to get the download URL
-                    return riversRef.getDownloadUrl();
-                }
-            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
-                @Override
-                public void onComplete(@NonNull Task<Uri> task) {
-                    if (task.isSuccessful()) {
-                        Uri downloadUri = task.getResult();
-                        pictureLink = downloadUri.toString();
-                        //firebaseFunction.insertBookInfo(pictureLink ,major,bookName, bookWriter,"지갑정보", resultList.get(0).getName());
-
-                    }
-                }
-            });
-            return null;
-        });
-    }
-
 }
